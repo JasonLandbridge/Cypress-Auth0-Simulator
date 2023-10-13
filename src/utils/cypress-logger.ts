01fbe9a094220c353f342b7e65bb7d86db9617b4
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-10-13 15:22:05 +0200

    fix(cypress-integration): Fixed logging, dont use cy.log!

--- src/utils/cypress-logger.ts
+++ src/utils/cypress-logger.ts
@@ -1,13 +1,10 @@
-export const makeCypressLogger =
-  (label: string, displayName: string = label) =>
-  (message: string) => {
-    Cypress.log({
-      name: label,
-      displayName,
-      message,
-    });
-  };
-
 export function cyLog(message: string, object: any | undefined = undefined) {
-  cy.log(message, JSON.stringify(object, null, 4));
+  console.log(message, JSON.stringify(object, null, 4));
+  // using cy.log() results in failed tests because it's not allowd in a Cypress promise
+  Cypress.log({
+    message: `${message} - ${JSON.stringify(object, null, 4)}`,
+    consoleProps: () => {
+      return { message, object };
+    },
+  });
 }
