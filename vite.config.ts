1d9f8e967e0a241dff059f5e37c5b0fb329afc76
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-10-13 17:25:29 +0200

    build(cypress-integration): The package is now build with Vite

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { builtinModules } from 'module';
+
+export const name = 'index';
+// Source: https://onderonur.netlify.app/blog/creating-a-typescript-library-with-vite/
+export default defineConfig({
+  build: {
+    target: 'ESNext',
+    sourcemap: true,
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name,
+      fileName: (format) => `${name}.${format}.js`,
+    },
+    // Source: https://github.com/vitejs/vite/issues/7821#issuecomment-1679416967
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+      external: [...builtinModules, /^node:/],
+    },
+  },
+
+  plugins: [dts()],
+});
