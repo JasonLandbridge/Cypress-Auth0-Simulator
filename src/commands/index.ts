bd27e402ae47c8810ec12f38966489cc11ffee79
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-10-13 12:19:15 +0200

    refactor(cypress-integration): Moved the Cypress custom command type to types.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,61 +1,35 @@
 import type { CreateSimulation, Person } from '../types';
-import { Auth0SDK } from "../types";
+import { Auth0SDK } from '../types';
 import type { Auth0Result } from 'auth0-js';
-import { getConfig } from "../utils";
-import { registerGeneralCommands } from "./general";
-import { registerAuth0JsCommands } from "./auth0-js";
-import { registerAuth0ReactCommands } from "./auth0-react";
-
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace Cypress {
-        interface Chainable<Subject> {
-            createSimulation(options?: CreateSimulation): Chainable<Subject>;
-
-            destroySimulation(): Chainable<Subject>;
-
-            login(person?: Partial<Person>): Chainable<string>;
-
-            logout(): Chainable<AUTWindow>;
-
-            given(attrs?: Partial<Person>): Chainable<Person>;
-
-            out<S = unknown>(msg: string): Chainable<S>;
-
-            getUserInfo(accessToken: string): Chainable<Person>;
-
-            getUserTokens(person: Person): Chainable<Auth0Result & { scope: string }>;
-
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            task<T>(event: string, args?: any, options?: Partial<Loggable & Timeoutable>): Chainable<T>;
-        }
-    }
-}
+import { getConfig } from '../utils';
+import { registerGeneralCommands } from './general';
+import { registerAuth0JsCommands } from './auth0-js';
+import { registerAuth0ReactCommands } from './auth0-react';
 
 function setupAuth0CypressCommands() {
-    let config = getConfig();
+  const config = getConfig();
 
-    let provider = config.sdk;
+  const provider = config.sdk;
 
-    registerGeneralCommands();
+  registerGeneralCommands();
 
-    switch (provider) {
-        case Auth0SDK.Auth0JS:
-            registerAuth0JsCommands();
-            break;
-        case Auth0SDK.Auth0Vue:
-            // Auth0 Vue uses the same commands as Auth0 React, using the native Auth0Vue SDK is not supported due to only 1 instance being allowed of the Auth0Vue SDK
-            registerAuth0ReactCommands();
-            break;
-        case Auth0SDK.Auth0React:
-            registerAuth0ReactCommands();
-            break;
-        case Auth0SDK.Auth0NextJS:
-            registerAuth0JsCommands();
-            break;
-        default:
-            throw new Error(`Unknown Auth0 SDK: ${provider}`);
-    }
+  switch (provider) {
+    case Auth0SDK.Auth0JS:
+      registerAuth0JsCommands();
+      break;
+    case Auth0SDK.Auth0Vue:
+      // Auth0 Vue uses the same commands as Auth0 React, using the native Auth0Vue SDK is not supported due to only 1 instance being allowed of the Auth0Vue SDK
+      registerAuth0ReactCommands();
+      break;
+    case Auth0SDK.Auth0React:
+      registerAuth0ReactCommands();
+      break;
+    case Auth0SDK.Auth0NextJS:
+      registerAuth0JsCommands();
+      break;
+    default:
+      throw new Error(`Unknown Auth0 SDK: ${provider}`);
+  }
 }
 
 setupAuth0CypressCommands();
--- src/types.ts
+++ src/types.ts
@@ -1,49 +1,82 @@
 import type { Slice } from '@effection/atom';
 import type { AuthOptions } from 'auth0-js';
 import type { Client, Simulation } from '@simulacrum/client';
+import { Auth0Result } from 'auth0-js';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable<Subject> {
+      createSimulation(options?: CreateSimulation): Chainable<Subject>;
+
+      destroySimulation(): Chainable<Subject>;
+
+      login(person?: Partial<Person>): Chainable<string>;
+
+      logout(): Chainable<AUTWindow>;
+
+      given(attrs?: Partial<Person>): Chainable<Person>;
+
+      out<S = unknown>(msg: string): Chainable<S>;
+
+      getUserInfo(accessToken: string): Chainable<Person>;
+
+      getUserTokens(person: Person): Chainable<Auth0Result & { scope: string }>;
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      task<T>(
+        event: string,
+        args?: any,
+        options?: Partial<Loggable & Timeoutable>,
+      ): Chainable<T>;
+    }
+  }
+}
 export const enum Auth0SDK {
-    Auth0JS = 'auth0-js',
-    Auth0Vue = 'auth0-vue',
-    Auth0React = 'auth0-react',
-    Auth0NextJS = 'nextjs-auth0',
+  Auth0JS = 'auth0-js',
+  Auth0Vue = 'auth0-vue',
+  Auth0React = 'auth0-react',
+  Auth0NextJS = 'nextjs-auth0',
 }
 
-export type TestState = Record<string, {
+export type TestState = Record<
+  string,
+  {
     client: Client;
     simulation?: Simulation;
-    person?: Person
-}>;
+    person?: Person;
+  }
+>;
 
 export type CreateSimulation = AuthOptions & { debug?: boolean };
 
 export interface Token {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    access_token: Record<string, any>;
-    expires_in: number;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    id_token: Record<string, any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  access_token: Record<string, any>;
+  expires_in: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  id_token: Record<string, any>;
 }
 
 export interface Person {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export type GetClientFromSpec = (spec: string) => Client;
 
 export interface EncryptPayload {
-    secret: string,
-    audience: string,
-    user: Person,
-    idToken?: string,
-    accessToken?: string,
-    accessTokenScope: string,
-    accessTokenExpiresAt: number
-    createdAt: number,
+  secret: string;
+  audience: string;
+  user: Person;
+  idToken?: string;
+  accessToken?: string;
+  accessTokenScope: string;
+  accessTokenExpiresAt: number;
+  createdAt: number;
 }
 
 export interface CommandMaker {
-    atom: Slice<TestState>;
-    getClientFromSpec: GetClientFromSpec;
+  atom: Slice<TestState>;
+  getClientFromSpec: GetClientFromSpec;
 }
